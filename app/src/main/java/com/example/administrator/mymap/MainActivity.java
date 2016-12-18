2b91d2505fdd42a6af7c2b1ea3d52a70e0634734
Huangjinlong <1440013637@qq.com>
2016-12-18 15:47:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/mymap/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.mymap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+    }
+}
